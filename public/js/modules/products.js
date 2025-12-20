aa362e25fffba9810a8b8bf882a8ef6d2d6c035f
mmd 2 <mhghaderi@yahoo.com>
2025-12-21 00:40:33 +0330

    fix footer and header

--- public/js/modules/products.js
+++ public/js/modules/products.js
@@ -1,44 +1,65 @@
 const box = document.getElementById("products-container");
-// const title = document.getElementById("title");
+const title = document.getElementById("cat-name");
 
 const params = new URLSearchParams(window.location.search);
 const categorySlug = params.get("cat");
 
-fetch(
-  `https://api.aradpacking.com/v1/categories/${encodeURIComponent(
-    categorySlug
-  )}`
-)
-  .then((res) => res.json())
-  .then((data) => {
-    // title.innerText = data.name; // از API
-    renderProducts(data.productList);
-  });
+if (!categorySlug) {
+  fetchAllProducts();
+} else {
+  fetchCategoryProducts(categorySlug);
+}
 
+/* ======================
+All Products
+====================== */
+async function fetchAllProducts() {
+  title.innerText = "همه محصولات";
+  box.innerHTML = "در حال بارگذاری...";
+
+  const res = await fetch("https://api.aradpacking.com/v1/products");
+  const products = await res.json();
+
+  renderProducts(products);
+}
+
+/* ======================
+  Selected Category Products
+====================== */
+function fetchCategoryProducts(slug) {
+  fetch(`https://api.aradpacking.com/v1/categories/${encodeURIComponent(slug)}`)
+    .then((res) => res.json())
+    .then((data) => {
+      title.innerText = "دستگاه‌های بسته‌بندی " + data.name;
+      renderProducts(data.productList);
+    });
+}
+
+/* ======================
+Cart Render
+====================== */
 function renderProducts(products) {
   box.innerHTML = "";
 
   products.forEach((p) => {
-    const img =
-      "https://api.aradpacking.com/" +
-      (p.images?.productCoverImage?.imagePath
-        ? p.images.productCoverImage.imagePath
-        : "../assets/images/placeholder.jpg");
-    console.log(img);
+    const mainImg = p.images?.machineCoverImage?.imagePath
+      ? "https://api.aradpacking.com/" + p.images.machineCoverImage.imagePath
+      : "assets/images/placeholder.jpg";
 
-    // const img = p.images?.productCoverImage?.imagePath
-    //   ? p.images.productCoverImage.imagePath
-    //   : "../assets/images/placeholder.jpg";
+    const hoverImg = p.images?.productCoverImage?.imagePath
+      ? "https://api.aradpacking.com/" + p.images.productCoverImage.imagePath
+      : mainImg;
 
     box.innerHTML += `
       <a href="product.html?slug=${encodeURIComponent(p.slug)}"
          class="product-card">
 
         <div class="image-box">
-          <img src="${img}" alt="${p.name}">
+          <img src="${mainImg}" class="img-main" alt="${p.name}">
+          <img src="${hoverImg}" class="img-hover" alt="${p.name}">
         </div>
 
-        <h3>دستگاه بسته بندی ${p.name}</h3>
+        <h3>دستگاه بسته‌بندی ${p.name}</h3>
       </a>
     `;
   });
